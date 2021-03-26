65da6e90b8642dd821b9b1dd60a59d424078cc2e
Maksims0230 <isip_m.a.stepankov@mpt.ru>
2021-03-26 20:57:14 +0300

    IronPython

---
+++ ConsolePythonNetApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using IronPython.Hosting;
+using System;
+
+namespace ConsolePythonNetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var engine = Python.CreateEngine();
+            var source = engine.CreateScriptSourceFromFile("calc.py");
+            var scope = engine.CreateScope();
+            source.Execute(scope);
+            var classCalculator = scope.GetVariable("myMatch");
+            var calculatorInstance = engine.Operations.CreateInstance(classCalculator);
+            Console.Write("Введите выражение: ");
+            var match = Console.ReadLine();
+            Console.WriteLine($"Результат: {calculatorInstance.calc(match)}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsolePythonNetApp/bin/Debug/calc.py
@@ -0,0 +1,21 @@
+class myMatch:
+    @staticmethod
+    def calc(math_str=""):
+        if myMatch.check(math_str):
+            return eval(math_str)
+        else:
+            return False
+
+    @staticmethod
+    def check(math=""):
+        if isinstance(math, str):
+            for char in math:
+                if not (char in '0987654321 -=+^.!*/'):
+                    return False
+        else:
+            return False
+        return True
+
+
+if __name__ == "__main__":
+    print(myMatch.calc())
